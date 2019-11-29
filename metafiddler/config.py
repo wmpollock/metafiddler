f73905e85d1d15819305dd8d3fdd451d246f8ead
William Pollock <wmpollock@gmail.com>
2019-11-29 14:53:30 -0800

    fully stubbed out load

--- metafiddler/config.py
+++ metafiddler/config.py
@@ -2,17 +2,20 @@ import pathlib
 import yaml
 
 
-
 # Things to save; 
 
-#current position
-
+# current position
 
 current_page = ''
-config_file = pathlib.Path.home() / "config.yaml"
+config_file = pathlib.Path.home() / ".metafiddlr.yaml"
 
 def init():
-    config = yaml.load(config_file)
+    try:
+        with open(config_file) as yaml_file:
+            config = yaml.load(yaml_file)
+        print("Loaded", config_file)
+    except:
+        print("No config in ", config_file)
 
 def save():
     print("Hi, I'd be saving :[")

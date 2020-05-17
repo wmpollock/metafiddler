af59adb9a817bd001dab4989ff5ac40cba10fb2f
Bill Pollock <william.pollock@lexisnexis.com>
2020-05-17 14:51:41 -0700

    More init-friendly.

--- metafiddler/config.py
+++ metafiddler/config.py
@@ -17,11 +17,8 @@ class MufiConfig:
             with open(self.config_file) as yaml_file:
                 self.config = yaml.load(yaml_file, Loader=yaml.FullLoader)
             logging.debug("Loaded " + self.config_file)
-
-
-
         except FileNotFoundError:
-            logging.debug("No config in " + self.config_file)
+            logging.warning("No config file " + self.config_file)
             # Hah, well, I guess we can start at the beginning then.
             self.current_page = "https://music.metafilter.com/8"
 
@@ -33,28 +30,24 @@ class MufiConfig:
     def current_page(self,url):
         self.config['current_page'] = url
 
+
     def playlist_config(self, playlist):
         if 'playlists' in self.config:
             if playlist in self.config:
                 return self.config['playlists'][playlist]
-            else:
-                print(playlist, "is not a configured list.")
-        else:
-            print("No configured playlists.")
-        return()
 
     def playlist_title(self, playlist):
         playlist=self.playlist_config(playlist)
-        if 'list_title' in playlist:
+        if playlist and 'list_title' in playlist:
             return playlist['list_title']
         else:
             return ""
             
     def playlist_id(self, playlist): 
         playlist=self.playlist_config(playlist)
-        if 'list_title' in playlist:
+        if playlist and 'list_title' in playlist:
             return playlist['list_id']
-        
+         
     def save(self):
         with open(self.config_file, 'w') as yaml_file:
             yaml.dump(self.config, yaml_file)

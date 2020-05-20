39f8ecf80909c5314d4537df26deaf5c603827d5
Bill Pollock <william.pollock@lexisnexis.com>
2020-05-19 20:12:33 -0700

    I don't get it winpty.

--- metafiddler/controller_implementation/windows_keyboard.py
+++ metafiddler/controller_implementation/windows_keyboard.py
@@ -29,6 +29,29 @@ bindings = {
         "desc": "↓"
 
     },
+
+    # I don't get this second set but winpty seemed to need it?  Seems crass.
+    b'\xe0' + b'M': {
+        "return":  metafiddler.event.NEXT,
+        # "desc": "arrrow-forward"
+        "desc": "→"
+    },
+    b'\xe0' + b'K': {
+        "return":  metafiddler.event.PREVIOUS,
+        # "desc": "arrow-back"
+        "desc": '←'
+    },
+    b'\xe0' + b'H': {
+        "return":  metafiddler.event.VOLUME_UP,
+        # "desc": "arrow-up"
+        "desc": "↑"
+    },
+    b'\xe0' + b'P': {
+        "return":  metafiddler.event.VOLUME_DOWN,
+        # "desc": "arrow-down"
+        "desc": "↓"
+    },
+
     # escape: may not want to print this bad boi 
     chr(27):  {
         "return":  metafiddler.event.STOP,
@@ -113,20 +136,22 @@ def poll():
         logging.critical("FATAL: this process is not a terminal.  Perhaps you need to prefix with winpty.")
         exit()
 
-    #keycode_signals = [b'\000', b'\xe0']
-    keycode_signal = b'\000'
+    # Had this commented out in favor of the singleton which didn't catch Winpty I guess?
+    keycode_signals = [b'\000', b'\xe0']
+    #keycode_signal = b'\000'
     try:
         if msvcrt.kbhit():
             
             ch = msvcrt.getch()
-            #print(ch.decode("utf-8"))
-            #if ch in keycode_signals:
             
             # Arrow keys have a prefix
-            if ch == keycode_signal:
-                key = ch + msvcrt.getch()
-            else:
+            if ch in keycode_signals:
+                x = msvcrt.getch()
+                key = ch + x
+            elif ch:
                 key = ch.decode("utf-8")
+            else:
+                logging.error("WHAT?!?  No key for my key?!?")
 
             if key in bindings:
                 return bindings[key]["return"]

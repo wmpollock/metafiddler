fa59022747eb4745bf858e03d4ed67843447930a
Bill Pollock <william.pollock@lexisnexis.com>
2020-06-16 20:30:42 -0700

    No login, know cry.

--- metafiddler/mechanise.py
+++ metafiddler/mechanise.py
@@ -24,41 +24,54 @@ def init():
     else:
         login()
 
+logged_in = 0
 def login():
     global jarfile
     global br
     global cj
-    user_name = input("Enter username:")
-    password = input("Enter password:")
-
-    br.open('https://login.metafilter.com')
-
-    br.select_form(action='logging-in.mefi')
+    global logged_in
     
-    br['user_name'] = user_name
-    br['user_pass'] = password
+    if not logged_in:
+        user_name = input("Enter username: ")
+        password = input("Enter password: ")
 
-    response = br.submit()
-    
-    # So at this point we should have a number of clues:
-    #   the response.read() text should has a li.profile .extra-label that contains the user_name
-    #   the cookie jar will contain USER_NAME
-    
-    cj.save(jarfile)
+        br.open('https://login.metafilter.com')
 
-def favorite(playlist_id, mufi_id):
+        br.select_form(action='logging-in.mefi')
+        
+        br['user_name'] = user_name
+        br['user_pass'] = password
+
+        response = br.submit()
+        print("Response code: ", response.code)
+        # So at this point we should have a number of clues:
+        #   the response.read() text should has a li.profile .extra-label that contains the user_name
+        #   the cookie jar will contain USER_NAME
+        
+        cj.save(jarfile)
+        logged_in = 1
+
+def playlist_add(playlist_id, mufi_id):
     global cj
-    #login()
-    response = br.open("https://music.metafilter.com/contribute/add_to_playlist.mefi?id=" + str(mufi_id))
+    login()
+    try:
+        response = br.open("https://music.metafilter.com/contribute/add_to_playlist.mefi?id=" + str(mufi_id))
+        print("Response code: ", response.code)
 
-    # if br.form == None:
-    #     #logging.critical("Did not receive page with form.")
-    #     print("Did not receive page with form.")
-    #     exit()
 
-    br.select_form(action='track-add.mefi')
-    br["playlist_id"] = str(playlist_id),
-    response = br.submit()
+        # if br.form == None:
+        #     #logging.critical("Did not receive page with form.")
+        #     print("Did not receive page with form.")
+        #     exit()
+
+        br.select_form(action='track-add.mefi')
+        br["playlist_id"] = str(playlist_id),
+        response = br.submit()
+        print("Response code: ", response.code)
+        return True
+    except:
+        logging.fatal("Could not submit to playlist.  We have no reason left to live.")
+        return False
 
     #print(response.read())
     cj.save(jarfile)

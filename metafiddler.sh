a9be43ec80acb1c49ff23f286f2988f9dc145a61
William Pollock <wmpollock@gmail.com>
2020-08-08 17:57:15 -0700

    dox

--- metafiddler.sh
+++ metafiddler.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# Windows helper script for those in various linux-like environs (shotout gitbash)
+
 # Python + Git BASH is a terrible combination for reasons I do not fully understand.  I half feel like 
 winpty py -u metafiddler.py

170a8e3fac8013d41526fbf59b997e731f9334ab
William Pollock <wmpollock@gmail.com>
2020-08-10 10:46:48 -0700

    embrace vscode

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -1,9 +1,14 @@
+"""Test for metafiddler.speech"""
+import unittest
+
 from metafiddler.speech import Speaker
 from metafiddler.config import MufiConfig
 
-if __name__ == "__main__":
-    config = MufiConfig()
-    pygame.mixer.init()
 
-    s = Speaker(config)
-    s.say("Audio test complete.")
+class TestConroller(unittest.TestCase):
+    """test functions for metafiddler.speech"""
+    def test_speech(self):
+        """Test a basic utterance"""
+        config = MufiConfig()
+        s = Speaker(config)
+        s.say("Audio test complete.")

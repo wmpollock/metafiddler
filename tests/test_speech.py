0cc7c2ec259c6891592c8f13e4afb0d390abfdb7
William Pollock <wmpollock@gmail.com>
2020-08-09 21:38:40 -0700

    Updated

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -1,10 +1,9 @@
-import metafiddler.speech
-from config import MufiConfig
+from metafiddler.speech import Speaker
+from metafiddler.config import MufiConfig
 
 if __name__ == "__main__":
     config = MufiConfig()
     pygame.mixer.init()    
 
     s = Speaker(config)
-    s.say("Aw ish")
-    print("done")
+    s.say("Audio test complete.")

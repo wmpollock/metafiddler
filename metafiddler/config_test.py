2743edd0ae403ae2871fde4334457a5c7fbcf239
William Pollock <wmpollock@gmail.com>
2020-08-09 19:41:31 -0700

    rename party - the things I do to make pylint happy retroactively

--- metafiddler/config_test.py
+++ metafiddler/config_test.py
@@ -1,10 +1,14 @@
-from metafiddler.config import MufiConfig
+"""Test for metafiddler.config"""
+import pprint
 import unittest
+from metafiddler.config import MufiConfig
+
 
 class TestConfig(unittest.TestCase):
+    """Test configuration methods"""
     def test_config(self):
+        """Test configuration load/setup"""
         config = MufiConfig()
-        import pprint
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(vars(config))
         print(config.playlist_id("playlist_b"))
--- metafiddler/input_test.py
+++ metafiddler/input_test.py
@@ -1,17 +1,20 @@
+"""Providing for metafiddler.input"""
+
 import unittest
 from metafiddler.input import Input
 from metafiddler.events.input import Event
 
 
 class TestConroller(unittest.TestCase):
-
+    """Test controller functions"""
     def test_controls(self):
-        input = Input()
-        e = input.poll()
+        """Test general control surface"""
+        user_input = Input()
+        e = user_input.poll()
         print("Gimme anyol input:")
         e = Event.NONE
         while e == Event.NONE:
-            e = input.poll()
+            e = user_input.poll()
 
         print("Got event", Event.describe(e))
         return True

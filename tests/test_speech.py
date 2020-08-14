37b24237170bac06ef3131a7acfaef25f3443cbb
William Pollock <wmpollock@gmail.com>
2020-08-13 20:21:22 -0700

    .

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -4,7 +4,6 @@ import unittest
 from metafiddler.speech import Speaker
 from metafiddler.config import MufiConfig
 
-
 class TestConroller(unittest.TestCase):
     """test functions for metafiddler.speech"""
     def test_speech(self):

d602b03b37af33517e157e05ff45ed3ba39acb4e
William Pollock <wmpollock@gmail.com>
2020-08-15 20:21:17 -0700

    pylint s( ^ ‿ ^)-b

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,27 +1,28 @@
 """Test for metafiddler.config"""
+import logging
 import pprint
 import unittest
-from metafiddler.config import MufiConfig
 from unittest.mock import patch, mock_open
-import logging
+from metafiddler.config import MufiConfig
+
 
 class TestConfig(unittest.TestCase):
     """Test configuration methods"""
 
     def setUp(self):
         logging.basicConfig(
-           level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s"
+            level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s"
         )
-    
+
 
     def test_config(self):
-        config = MufiConfig()
         """Test configuration load/setup"""
-        
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(vars(config))
+        config = MufiConfig()
+
+        pretty = pprint.PrettyPrinter(indent=4)
+        pretty.pprint(vars(config))
         print(config.playlist_id("playlist_b"))
-        print("Current page:", config.current_page)            
+        print("Current page:", config.current_page)
         print("Song save dir:", config.song_save_dir)
 
     def test_update(self):

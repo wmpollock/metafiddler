e0e41f2699eae5b83ee076426b287adbe17531ad
William Pollock <wmpollock@gmail.com>
2020-08-23 18:54:38 -0700

    meh, not mocking

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,9 @@
 """Test for metafiddler.config"""
 import logging
+import os
 import pprint
+import requests
+import requests_mock
 import unittest
 from unittest.mock import patch, mock_open
 from metafiddler.config import MufiConfig
@@ -24,17 +27,30 @@ class TestConfig(unittest.TestCase):
         print(config.playlist_id("playlist_b"))
         print("Current page:", config.current_page_url)
         print("Song save dir:", config.song_save_dir)
+        self.assertIsNotNone(config.current_page_url)
 
     def test_update(self):
         """Test whether update works"""
         config = MufiConfig()
+        config.current_page_post_url = "mock://foo.foo"
         # https://github.com/otrabalhador/python-testing-by-examples/blob/master/docs/en/mocking/examples/reading-writing-on-files.md#writing-on-files
+
+        session = requests.Session()
+        adapter = requests_mock.Adapter()
+        session.mount('mock://', adapter)
+
+        adapter.register_uri('GET', 'mock://foo.foo', text='data')
         with patch('builtins.open', mock_open()) as mocked_file:
-            # This is a pretty janky case since we're just laying it down
-            # but as its halfassedly mocked ATM its hitting live backend :O
+
+            # Make sure we're testing safely...
+            original_config_mtime = os.path.getmtime(config.config_file)
 
             test_value = config.current_page_url
             print(f"Setting current page to {test_value}")
+
             config.current_page_url = test_value
             mocked_file.assert_called_once_with(config.state_file, mode='w')
-            mocked_file().write.assert_called_once_with(test_value)
+            self.assertTrue(mocked_file().write.assert_called_once_with(test_value))
+
+            new_config_mtime = os.path.getmtime(config.config_file)
+            self.assertEqual(original_config_mtime, new_config_mtime)

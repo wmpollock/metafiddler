188cde6df3862336c8ddb1cd21728bd282b7e900
William Pollock <wmpollock@gmail.com>
2020-09-13 21:20:46 -0700

    go fuller interface

--- metafiddler/controller/interface.py
+++ metafiddler/controller/interface.py
@@ -29,25 +29,10 @@ class ControllerInterface:
     """ Abstract input interface class """
     bindings = {}
 
-    def print_bindings(self):
-        """ Display the bindings for a given key """
-        table = []
-        bindings = self.bindings
-        for key in bindings.keys():
-            # Haha, since we use the labels I guess we don't need a dictionary to look these up (yikes...)
-
-            if "description" in bindings[key]:
-                label = bindings[key]["description"]
-            else:
-                label = key
-
-            # ?print("\t%-15s %s" %(label + ":", bindings[key]["return"]))
-            table.append([label, bindings[key]["return"].description])
-
-        print(tabulate([["Keyboard mapping"]], tablefmt="github"))
-
-        print(tabulate(table, tablefmt="grid"))
-
     def poll(self):
+        """ Interface class """
+        raise NotImplementedError()
+
+    def print_bindings(self):
         """ Interface class """
         raise NotImplementedError()

8bd29b7c37d48d3abc23d38d649988d8a647178e
William Pollock <wmpollock@gmail.com>
2020-09-19 15:34:04 -0700

    sketch before I start getting formal and need to undo :O

---
+++ tests/test_controller_unix.py
@@ -0,0 +1,31 @@
+""" Unittest for Unix interfaces """
+
+import unittest
+from unittest.patch import Mock
+
+class EvEvent():
+    def __init__:
+        code=None
+        type=None
+        val=None
+
+def gamepad_event(
+    fake_gamepad = Mock(side_effect=[self.retval])
+    gamepad_obj = Mock(
+        gamepads=[fake_gamepad]
+    )
+
+
+class TestGamepad(unittest.Testcase)
+    """ Test the unix gamepad """
+    reval=None
+
+    def setUp(self):
+
+    def test_poll(self):
+        # A
+        gamepad_event(04, 04, 589826)
+        # B
+        gamepad_event(04, 04, 589826)
+        # X
+        gamepad_event(04, 04, 589825)

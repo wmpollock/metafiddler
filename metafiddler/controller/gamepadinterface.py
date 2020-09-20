f39b3ba9e4882abbbcf1ff23c9fe4119b324a572
William Pollock <wmpollock@gmail.com>
2020-09-19 18:02:33 -0700

    fleshed out types

--- metafiddler/controller/gamepadinterface.py
+++ metafiddler/controller/gamepadinterface.py
@@ -8,17 +8,20 @@ class GamepadInterface():
     # PLAYER BUTTONS
     # -----------------------------------------------------------------------------
     bindings = {
-        "a": {"return": InputEvent.PLAYLIST_A},
-        "b": {"return": InputEvent.PLAYLIST_B},
-        "x": {"return": InputEvent.PLAYLIST_X},
-        "y": {"return": InputEvent.PLAYLIST_Y},
-        "left": {"return": InputEvent.NEXT},
-        "right": {"return": InputEvent.SEEK_FORWARD},
-        "start": {"return": InputEvent.PLAY},
-        "select": {"return": InputEvent.STOP},
-        "shoulder_l": {"return": InputEvent.SEEK_FORWARD},
-        "shoulder_r": {"return": InputEvent.SEEK_BACK},
-
+        "a":  InputEvent.PLAYLIST_A,
+        "b":  InputEvent.PLAYLIST_B,
+        "x":  InputEvent.PLAYLIST_X,
+        "y":  InputEvent.PLAYLIST_Y,
+        "left":  InputEvent.NEXT,
+        "right":  InputEvent.SEEK_FORWARD,
+        "start":  InputEvent.PLAY,
+        "select":  InputEvent.STOP,
+        "shoulder_r":  InputEvent.SEEK_FORWARD,
+        "shoulder_l":  InputEvent.SEEK_BACK,
+        "up": InputEvent.VOLUME_UP,
+        "down": InputEvent.VOLUME_DOWN,
+        "left": InputEvent.PREVIOUS,
+        "right": InputEvent.NEXT
     }
 
     @classmethod

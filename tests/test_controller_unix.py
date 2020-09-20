612ec09fe5a760758e481b26f90dbec9ffa50886
William Pollock <wmpollock@gmail.com>
2020-09-19 18:03:00 -0700

    cvg for new events

--- tests/test_controller_unix.py
+++ tests/test_controller_unix.py
@@ -3,34 +3,99 @@
 """ Unittest for Unix interfaces """
 
 import unittest
-from unittest.patch import Mock
+from unittest.mock import Mock
+
+from metafiddler.controller.unix.gamepad import Gamepad, GamepadInterface
+from metafiddler.input_events import InputEvent
 
 class EvEvent():
-    def __init__:
-        code=None
-        type=None
-        val=None
-
-def gamepad_event(
-    fake_gamepad = Mock(side_effect=[self.retval])
-    gamepad_obj = Mock(
-        gamepads=[fake_gamepad]
-    )
+    """ Mock up the bogus event """
+    def __init__(self, code, evtype, val):
+        self.code = code
+        self.type = evtype
+        self.val = val
+    def __str__(self):
+        return f"Code: {self.code} Type: {self.type} Value: {self.val}"
 
 
-class TestGamepad(unittest.Testcase)
+
+
+def gamepads(code, evtype, val):
+    fake_event = EvEvent(code,evtype,val)
+    fake_gamepad =  Mock(**{"read_one.side_effect":[fake_event, None]})
+    # fake_gamepad =  Mock(side_effect=[fake_event])
+
+    return Mock(spec=Gamepad, **{
+        "gamepads": [fake_gamepad]
+    })
+
+
+
+
+class TestGamepad(unittest.TestCase):
     """ Test the unix gamepad """
     reval=None
 
-    def setUp(self):
-
     def test_poll(self):
+        self.assertEqual(
+            InputEvent.NONE, 
+            Gamepad.translate_event(GamepadInterface(), EvEvent(None, None, None))
+        )
         # A
-        gamepad_event(04, 04, 589826)
-        # B
-        gamepad_event(04, 04, 589826)
-        # X
-        gamepad_event(04, 04, 589825)
+        self.assertEqual(
+            InputEvent.PLAYLIST_A, 
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589826)))
+        self.assertEqual(
+            InputEvent.PLAYLIST_B,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589827))
+        )
+
+        self.assertEqual(
+            InputEvent.PLAYLIST_X,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589825))
+        )
+
+
+        self.assertEqual(
+            InputEvent.PLAYLIST_Y,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589828))
+        )
+        
+        # Start button
+        self.assertEqual(
+            InputEvent.PLAY,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589834))
+        )
+        self.assertEqual(
+            InputEvent.STOP,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589833))
+        )
+        self.assertEqual(
+            InputEvent.SEEK_BACK,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589829))
+        )
+        self.assertEqual(
+            InputEvent.SEEK_FORWARD,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(4, 4, 589830))
+        )
+        self.assertEqual(
+            InputEvent.VOLUME_DOWN,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(1, 3, 255))
+        )
+        self.assertEqual(
+            InputEvent.VOLUME_UP,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(1, 3, 0))
+        )
+        self.assertEqual(
+            InputEvent.PREVIOUS,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(0, 3, 0))
+        )
+        self.assertEqual(
+            InputEvent.NEXT,
+            Gamepad.translate_event(GamepadInterface(), EvEvent(0, 3, 255))
+        )
+
+        
 
 if __name__ == "__main__":
     unittest.main()
